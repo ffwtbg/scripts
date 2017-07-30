01e9550728484b01dcbf0d0b907c5ebee78e0662
Kakinger <jan@zueschen.eu>
2017-07-30 09:36:26 +0200

    added all firmware branches

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 # variables
-BRANCH="experimental"
 VERSION="0.8.11.1"
 BROKEN="1"
 GLUONBRANCH="v2016.2.6"
@@ -22,6 +21,9 @@ rm -rf site_ffwtbg
 git clone https://github.com/ffwtbg/site.git -b $GLUONBRANCH site_ffwtbg
 mkdir site
 cp -R site_ffwtbg/site/* site/
+
+# set Firmware Branch
+BRANCH="stable"
 
 #################
 ### Kernstadt ###
@@ -165,7 +167,454 @@ tar cfzv /media/sdb1/html/hoehendoerfer_${VERSION}_${BRANCH}.tar.gz output/image
 
 # clean the folders
 rm -rf output/*
-rm -rf site/*
+rm -f site/site.conf
+
+#####################################################################
+# set Firmware Branch
+BRANCH="experimental"
+
+#################
+### Kernstadt ###
+#################
+# copy site config
+cp site_ffwtbg/site-kernstadt/site.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/kernstadt_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+#################
+###  Doerfer  ###
+#################
+# copy site config
+cp site_ffwtbg/site-doerfer/site.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/doerfer_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+##################
+###  Medebach  ###
+##################
+# copy site config
+cp site_ffwtbg/site-medebach/site.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/medebach_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+##################
+### Hallenberg ###
+##################
+# copy site config
+cp site_ffwtbg/site-hallenberg/site.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/hallenberg_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+######################
+### Siedlinghausen ###
+######################
+# copy site config
+cp site_ffwtbg/site-sdlh/site.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/siedlinghausen_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+#####################
+### Hoehendoerfer ###
+#####################
+# copy site config
+cp site_ffwtbg/site-hoehendoerfer/site.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/hoehendoerfer_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+#####################################################################
+# set Firmware Branch
+BRANCH="custom_ch01"
+
+#################
+### Kernstadt ###
+#################
+# copy site config
+cp site_ffwtbg/site-kernstadt/site_custom_ch01.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/kernstadt_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+#################
+###  Doerfer  ###
+#################
+# copy site config
+cp site_ffwtbg/site-doerfer/site_custom_ch01.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/doerfer_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+##################
+###  Medebach  ###
+##################
+# copy site config
+cp site_ffwtbg/site-medebach/site_custom_ch01.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/medebach_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+##################
+### Hallenberg ###
+##################
+# copy site config
+cp site_ffwtbg/site-hallenberg/site_custom_ch01.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/hallenberg_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+######################
+### Siedlinghausen ###
+######################
+# copy site config
+cp site_ffwtbg/site-sdlh/site_custom_ch01.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/siedlinghausen_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+#####################
+### Hoehendoerfer ###
+#####################
+# copy site config
+cp site_ffwtbg/site-hoehendoerfer/site_custom_ch01.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/hoehendoerfer_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+#####################################################################
+# set Firmware Branch
+BRANCH="custom_ch11"
+
+#################
+### Kernstadt ###
+#################
+# copy site config
+cp site_ffwtbg/site-kernstadt/site_custom_ch11.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/kernstadt_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+#################
+###  Doerfer  ###
+#################
+# copy site config
+cp site_ffwtbg/site-doerfer/site_custom_ch11.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/doerfer_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+##################
+###  Medebach  ###
+##################
+# copy site config
+cp site_ffwtbg/site-medebach/site_custom_ch11.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/medebach_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+##################
+### Hallenberg ###
+##################
+# copy site config
+cp site_ffwtbg/site-hallenberg/site_custom_ch11.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/hallenberg_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+######################
+### Siedlinghausen ###
+######################
+# copy site config
+cp site_ffwtbg/site-sdlh/site_custom_ch11.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/siedlinghausen_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+#####################
+### Hoehendoerfer ###
+#####################
+# copy site config
+cp site_ffwtbg/site-hoehendoerfer/site_custom_ch11.conf site/site.conf
+
+# make update
+echo "Running 'make update'"
+make update &> log/mkupdate.log
+
+# make
+for TARGET in $(echo $TARGETS | sed "s/,/ /g")
+do
+    echo "$TARGET"
+	make GLUON_TARGET=${TARGET} -j12 BROKEN=${BROKEN} GLUON_BRANCH=$BRANCH
+done
+
+# create zip archive
+tar cfzv /media/sdb1/html/hoehendoerfer_${VERSION}_${BRANCH}.tar.gz output/images
+
+# clean the folders
+rm -rf output/*
+rm -f site/site.conf
+
+
+###########################################################################################
 
 # end time logging
 after=$(date +%s)
